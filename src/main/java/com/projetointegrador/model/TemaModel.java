ff42eb53e524b91fc6da2c39751cdc4d19f3f282
Josiane <jcarolliny@gmail.com>
2021-12-06 10:56:54 -0300

    @JosianeCaroliny [Task 4: Criando atributos da model Tema]

--- src/main/java/com/projetointegrador/model/TemaModel.java
+++ src/main/java/com/projetointegrador/model/TemaModel.java
@@ -1,5 +1,13 @@
 package com.projetointegrador.model;
 
+import com.projetointegrador.model.enums.EscolaridadeEnum;
+import com.projetointegrador.model.enums.TemaEnum;
+
 public class TemaModel {
+	
+	private Long id_tema;
+	private TemaEnum tema_enum;
+	private String subtema;
+	private EscolaridadeEnum escolaridade;
 
 }
