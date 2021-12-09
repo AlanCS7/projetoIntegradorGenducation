7ec5f4d8ac32090dd5174f4fb945ea926cc3f0d2
AlanCS7 <alanclash2541@gmail.com>
2021-12-09 11:18:56 -0300

    @AlanCS7 [Task: Adicionando o método GetMapping para buscar por tema e escolaridade]

--- src/main/java/com/projetointegrador/controller/TemaController.java
+++ src/main/java/com/projetointegrador/controller/TemaController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetointegrador.model.TemaModel;
+import com.projetointegrador.model.enums.EscolaridadeEnum;
+import com.projetointegrador.model.enums.TemaEnum;
 import com.projetointegrador.repository.TemaRepository;
 
 @RestController
@@ -27,4 +30,14 @@ public class TemaController {
 
 	}
 
+	@GetMapping("/{temaEnum}")
+	public ResponseEntity<List<TemaModel>> getTema(@PathVariable(value = "temaEnum") TemaEnum temaEnum) {
+		return ResponseEntity.ok(repository.findAllByTemaEnum(temaEnum));
+	}
+	
+
+	@GetMapping("/escolaridade/{escolaridadeEnum}")
+	public ResponseEntity<List<TemaModel>> getEscolaridade(@PathVariable(value = "escolaridadeEnum") EscolaridadeEnum escolaridadeEnum) {
+		return ResponseEntity.ok(repository.findAllByEscolaridade(escolaridadeEnum));
+	}
 }
