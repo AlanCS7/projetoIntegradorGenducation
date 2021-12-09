7faf3462aa51ede8bdf015227159ba36814ca5df
fabricior0cha <fabriciorochapalmeira@gmail.com>
2021-12-09 11:17:18 -0300

    @fabricior0cha [Task: Adicionando Delete]

--- src/main/java/com/projetointegrador/controller/TemaController.java
+++ src/main/java/com/projetointegrador/controller/TemaController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class TemaController {
 	public ResponseEntity<List<TemaModel>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable (value = "id") Long id) {
+		repository.deleteById(id);
 	}
 
 }
