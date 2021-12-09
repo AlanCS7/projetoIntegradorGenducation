9a9a8336b207e06592da0a263d6e7ac682653455
fabricior0cha <fabriciorochapalmeira@gmail.com>
2021-12-09 10:24:54 -0300

    @fabricior0cha [Task: Criando Repository de Tema]

---
+++ src/main/java/com/projetointegrador/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package com.projetointegrador.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetointegrador.model.TemaModel;
+
+@Repository
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
+
+	public List<TemaModel> findAllByTemaEnumContainingIgnoreCase(String tema);
+
+}
