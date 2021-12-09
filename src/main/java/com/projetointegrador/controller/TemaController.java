628a3551aa5ad4ff872f99ce40edfc263d0a8413
wesleycalle <wesleyninaja@gmail.com>
2021-12-09 11:19:05 -0300

    @WesNinaja [Task: Adicionando método GET MAPPING para buscar pela id]

--- src/main/java/com/projetointegrador/controller/TemaController.java
+++ src/main/java/com/projetointegrador/controller/TemaController.java
@@ -3,11 +3,14 @@ package com.projetointegrador.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.projetointegrador.model.TemaModel;
 import com.projetointegrador.repository.TemaRepository;
@@ -21,10 +24,18 @@ public class TemaController {
 	@Autowired
 	private TemaRepository repository;
 
+	// GET ALL
 	@GetMapping
 	public ResponseEntity<List<TemaModel>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 
+	}
+
+	// GET BY ID
+	@GetMapping("/{id}")
+	public ResponseEntity<TemaModel> getById(@PathVariable(value = "id") long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não encontrado"));
 	}
 
 }
