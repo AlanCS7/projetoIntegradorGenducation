3ca157a741fb7d8c06d01fad5bbb758d02a354cb
unknown <gui.albuquerque.gui@gmail.com>
2021-12-09 10:55:41 -0300

    @guiAdaCruz [Task: Criando o método Post]

--- src/main/java/com/projetointegrador/controller/TemaController.java
+++ src/main/java/com/projetointegrador/controller/TemaController.java
@@ -2,12 +2,18 @@ package com.projetointegrador.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.projetointegrador.model.TemaModel;
 import com.projetointegrador.repository.TemaRepository;
@@ -24,7 +30,11 @@ public class TemaController {
 	@GetMapping
 	public ResponseEntity<List<TemaModel>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
-
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<TemaModel> post(@Valid @RequestBody TemaModel tema){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));			
 	}
 
 }
