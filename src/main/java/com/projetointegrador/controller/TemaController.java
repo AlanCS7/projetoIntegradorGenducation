c910edaeb31e8fae257b63109d424b156751e27c
Josiane <jcarolliny@gmail.com>
2021-12-09 10:55:45 -0300

    @JosianeCaroliny [Task: Criando método Put]

--- src/main/java/com/projetointegrador/controller/TemaController.java
+++ src/main/java/com/projetointegrador/controller/TemaController.java
@@ -2,10 +2,15 @@ package com.projetointegrador.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +30,11 @@ public class TemaController {
 	public ResponseEntity<List<TemaModel>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<TemaModel> put(@Valid @RequestBody TemaModel tema) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
 	}
 
 }
