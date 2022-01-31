7db644c2624d2632b6dc78dbceb427ab23304b47
Alan Carlos <alanclash2541@gmail.com>
2022-01-31 11:00:12 -0300

    Autorizando requisições para cadastrar tema e postagem

--- src/main/java/com/projetointegrador/security/BasicSecurityConfig.java
+++ src/main/java/com/projetointegrador/security/BasicSecurityConfig.java
@@ -34,6 +34,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers(HttpMethod.POST, "/Genducation/usuarios/cadastrar").permitAll()
 		.antMatchers(HttpMethod.PUT, "/Genducation/usuarios/logar").permitAll()
 		.anyRequest().authenticated()
